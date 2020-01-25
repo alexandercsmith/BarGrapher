4681d7d2df20d5d97d44c272420f689e7da19bf7
Alexander Smith <acsmith.dev@gmail.com>
2020-01-24 22:19:59 -0500

    add bargraph view

---
+++ BarGrapher/BarGraph.swift
@@ -0,0 +1,23 @@
+//
+//  BarGraph.swift
+//  BarGrapher
+//
+//  Created by Alexander Smith on 1/24/20.
+//  Copyright © 2020 Alexander Smith. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct BarGraph: View {
+  var body: some View {
+    Text("Bar Graph")
+  }
+}
+
+
+struct BarGraph_Previews: PreviewProvider {
+  static var previews: some View {
+    BarGraph()
+  }
+}
