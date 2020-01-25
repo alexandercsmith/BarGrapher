5747238ad9dcc6f6696ad437b0928a4b29a31dc6
Alexander Smith <acsmith.dev@gmail.com>
2020-01-25 11:17:35 -0500

    add to content view

--- BarGrapher/ContentView.swift
+++ BarGrapher/ContentView.swift
@@ -9,13 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+  var body: some View {
+    BarGraph(reports: Report.all())
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
